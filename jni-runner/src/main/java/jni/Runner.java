7eb0137a854357875c617cff70004b7f90677f5b
stwind <stwindfy@gmail.com>
2020-05-13 22:24:29 +0800

    fix: Add gradle wrapper jar

--- jni-runner/src/main/java/jni/Runner.java
+++ jni-runner/src/main/java/jni/Runner.java
@@ -2,9 +2,6 @@ package jni;
 
 public class Runner {
     public static void main(String[] args) {
-        // Boolean res = LibraryLoader.load(Tools.class, "tools");
-        // System.out.println(res);
-
         Tools tools = new Tools();
         System.out.println(tools.foo() + tools.bar());
     }
